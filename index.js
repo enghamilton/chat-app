ba500e67de1046ad47dd163f52ffc292bea2e9f9
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-14 06:48:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('client'));
+//app.use(express.static('client'));
+app.use(express.static('rest'));
 
 server.listen(PORT, function() {
   console.log('Chat server running');
