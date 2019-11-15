4d005bf5b7d02c7f334f69c6b1fe4489f39fc420
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-15 07:08:35 -0300

    Update main.js

--- rest/main.js
+++ rest/main.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+/*
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
       res.end( data );
    });
 })
+*/
+app.get('/', (req, res) => res.json({ 'pid': 1, 'user':'user 01', 'price':'20.00', 'description':'first description' }))
 
 var server = app.listen(8081, function () {
    var host = server.address().address
