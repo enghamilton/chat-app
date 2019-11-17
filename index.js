b4831b06d0e7bdfa0965e40fdad064be62e3a837
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-17 09:54:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('client'));
+//app.use(express.static('client'));
+app.get('/', function (req, res) {
+  //res.send('Hello World!');
+  //res.json({ username: 'user 01' })
+  res.json({ "products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1 })
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 server.listen(PORT, function() {
   console.log('Chat server running');
-});
-
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
 });
